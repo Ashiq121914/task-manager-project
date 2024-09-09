0ab40721e22e7bcba3b1df39db400db9bfb6afa3
Ashiq121914 <ashiq121914@gmail.com>
2024-09-09 18:13:56 +0600

    code verification

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -92,8 +92,25 @@ export const EmailVerify = async (req, res) => {
   }
 };
 
+// code verify
 export const CodeVerify = async (req, res) => {
-  return res.json({ status: "success" });
+  try {
+    let reqBody = req.body;
+    let data = await Users.findOne({
+      email: reqBody["email"],
+      otp: reqBody["otp"],
+    });
+    if (data == null) {
+      return res.json({ status: "fail", Message: "Wrong Verification Code" });
+    } else {
+      return res.json({
+        status: "success",
+        Message: "Verification successfully",
+      });
+    }
+  } catch (e) {
+    return res.json({ status: "fail", Message: e.toString() });
+  }
 };
 
 export const ResetPassword = async (req, res) => {
